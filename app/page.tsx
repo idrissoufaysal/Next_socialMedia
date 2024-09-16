87ec4d306230863639477fe8102753af620d010a
idrissou <idrissoufaysal93@gmail.com>
2024-09-16 22:56:42 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="text-primary-600 text-xl m-20">Home</div>
+      <Button className="bg-light-4"> ajouter</Button>
+    </div>
+  );
 }
