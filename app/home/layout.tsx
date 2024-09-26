1256e5ca0c0edbd4c1881f7218527d57d0e3165e
idrissou <idrissoufaysal93@gmail.com>
2024-09-26 22:33:09 +0000

    w

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -2,15 +2,15 @@ import LeftBar from "@/components/shared/leftBar";
 import Navbar from "@/components/shared/navbar";
 
 export default function HomeLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-     <div className="flex flex-col">
-     <Navbar/>
-     {children}
-     <LeftBar/>
-     </div>
-    );
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="md:flex w-full ">
+      <Navbar />
+      <LeftBar />
+      <section className="flex flex-1 h-full">{children}</section>
+    </div>
+  );
+}
