450bd3c96bd00872121a3467c607145bf3eee6ef
idrissou <idrissoufaysal93@gmail.com>
2024-10-04 16:20:38 +0000

    aa

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import Loader from "@/components/shared/Loader";
+import React from "react";
 
 export default function HomePage() {
+  const isPostLoading = false;
+  const posts = null;
+
   return (
-    <div>HomePage</div>  )
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full"> Home Feed</h2>
+          {isPostLoading && !posts ? <Loader /> : 
+          <ul className="flex flex-1 flex-col gap-9  w-full">
+            Text
+             </ul>}
+        </div>
+      </div>
+    </div>
+  );
 }
