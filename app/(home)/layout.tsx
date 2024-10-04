98d82d31f3edb03347692cc9aa567ea05200d0d8
idrissou <idrissoufaysal93@gmail.com>
2024-10-04 11:42:29 +0000

    aa

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,19 @@
+// app/(home)/layout.tsx
+import Bottombar from "@/components/shared/Bottombar";
+import LeftBar from "@/components/shared/leftBar";
+import Topbar from "@/components/shared/Topbar";
+
+export default function HomeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="md:flex w-full h-screen">
+      <Topbar />
+      <LeftBar />
+      <section className="flex flex-1 h-full">{children}</section>
+      <Bottombar />
+    </div>
+  );
+}
